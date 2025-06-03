6e347f7701ec560ca22bb39466a5c9d398eaf95a
Jerucho <amadorjeremias2006@gmail.com>
2025-06-02 19:42:48 -0500

    Primer commit del proyecto JPA con Spring

---
+++ src/main/java/com/sharp/clinica_solar/ClinicaSolarApplication.java
@@ -0,0 +1,13 @@
+package com.sharp.clinica_solar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaSolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaSolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharp/clinica_solar/ClinicaSolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharp.clinica_solar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaSolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
